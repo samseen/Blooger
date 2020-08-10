3abe812c1d9b7c7d950e987ec9e14706fae29616
Samson Akanbi <samson.akanbi@outlook.com>
2020-08-10 23:16:55 +0100

    Added custom arrows for owl carousel with js

--- js/main.js
+++ js/main.js
@@ -9,6 +9,13 @@ $(document).ready(function(){
     })
 
     // owl-carousel for the blog
-    $('.owl-carousel').owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    });
 
 });
